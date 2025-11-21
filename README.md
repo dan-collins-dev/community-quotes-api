612cff68d975da49f69b4dd19a016788c86c023d
Dan Collins <dan.collins.dev@gmail.com>
2025-11-20 21:32:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Community Quotes API
---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const quotes = ["This project is going to be streets ahead"];
+
+app.get("/", (req, res) => {
+  res.status(200).json(quotes);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
