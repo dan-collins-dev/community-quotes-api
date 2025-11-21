e974a700acd9a6f8293139683e7e534dd7ac6fe4
Dan Collins <dan.collins.dev@gmail.com>
2025-11-20 22:22:50 -0500

    fixed reading from json file issue

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const quotes = JSON.parse(fs.readFileSync("./quotes.json", "utf-8"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const quotesPath = path.join(__dirname, "quotes.json");
+const quotes = JSON.parse(fs.readFileSync(quotesPath, "utf-8"));
 
 app.get("/", (req, res) => {
   res.status(200).json(quotes);
