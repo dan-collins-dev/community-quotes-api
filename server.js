2f824bbb590699fcb94ee23ad53476a405a8ee7a
Dan Collins <dan.collins.dev@gmail.com>
2025-11-23 14:56:09 -0500

    simple front end added and new routes added

--- server.js
+++ server.js
@@ -11,8 +11,15 @@ const __dirname = path.dirname(__filename);
 const quotesPath = path.join(__dirname, "quotes.json");
 const quotes = JSON.parse(fs.readFileSync(quotesPath, "utf-8"));
 
-app.get("/", (req, res) => {
+app.use(express.static("./public"));
+
+app.get("/api", (req, res) => {
   res.status(200).json(quotes);
+});
+
+app.get("/api/random", (req, res) => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  res.status(200).json(quotes[randomIndex]);
 });
 
 app.listen(PORT, () => {
